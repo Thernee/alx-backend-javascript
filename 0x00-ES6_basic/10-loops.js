66c63826d1f0018aaf63e9ca2edb77340ed363ae
Abubakar Adam Sani <Athernee@gmail.com>
2024-01-03 23:03:34 +0100

    updated 10-loops.js with ES6 syntax

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,7 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const modifiedArray = array;
+  for (const value of array) {
+    modifiedArray[array.indexOf(value)] = appendString + value;
+  }
+  return array;
+}
