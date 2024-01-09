6cab1ed24c044128bcbc65830279c1b236ecb5e2
Abubakar Adam Sani <Athernee@gmail.com>
2024-01-09 12:53:54 +0100

    Add: 12-createReportObject.js

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,13 @@
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments (employeesList) {
+      // return Object.keys(employeesList).length;
+
+      // Another way to write the commented code:
+      return Object.entries(employeesList).length;
+    }
+  }
+}
