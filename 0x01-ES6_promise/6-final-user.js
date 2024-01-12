299ab2d9fe9ac0f6174428d1d2a7a0c4381d9e67
Abubakar Adam Sani <Athernee@gmail.com>
2024-01-12 17:31:05 +0100

    fix: 5-photo-reject.js, 6-final-user.js

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,10 +1,15 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled(
-    uploadPhoto(fileName).catch(() => null),
-    signUpUser(firstName, lastName),
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  try {
+    const values = await Promise.allSettled([
+      uploadPhoto(fileName),
+      signUpUser(firstName, lastName),
+    ]);
 
-  ).then((values) => console.log(values)).catch(() => null);
+    return values;
+  } catch (err) {
+    return err;
+  }
 }
