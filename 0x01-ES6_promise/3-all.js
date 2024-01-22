c9663558070eb17effd4673c34bf28c33e3aba38
Abubakar Adam Sani <Athernee@gmail.com>
2024-01-22 21:15:43 +0100

    Updated 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -4,7 +4,7 @@ export default function handleProfileSignup() {
   return uploadPhoto().then(({ body }) => {
     createUser()
       .then(({ firstName, lastName }) => {
-        console.log(body, firstName, lastName);
+        console.log(`${body} ${firstName} ${lastName}`);
       }).catch(() => console.log('Signup system offline'));
   }).catch(() => console.log('Signup system offline'));
 }
