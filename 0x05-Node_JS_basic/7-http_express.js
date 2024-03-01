171100ae4fc12faa9a959d070c7d7b4b0c8c3d5f
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-01 19:12:27 +0100

    fix: wrong db error response

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -12,10 +12,10 @@ app.get('/', (req, res) => {
 app.get('/students', (req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
+  res.write('This is the list of our students\n');
 
   countStudents(process.argv[2])
     .then((result) => {
-      res.write('This is the list of our students\n');
       res.write(`Number of students: ${result.totalStudents}\n`);
       res.write(`Number of students in CS: ${result.csStudents.length}. List: ${result.csStudents.join(', ')}\n`);
       res.end(`Number of students in SWE: ${result.sweStudents.length}. List: ${result.sweStudents.join(', ')}`);
