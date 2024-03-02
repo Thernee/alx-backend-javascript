3b7bec88ba64c949cf6e33f45e3129fa798200ff
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-02 14:51:49 +0100

    updated test suite

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,12 +1,16 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function () {
-  it('should return the sum of two rounded off numbers', function () {
+describe('calculateNumber', () => {
+  it('should check the sum of two rounded off numbers', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
     assert.strictEqual(calculateNumber(1.4, 3), 4);
+  });
+  it('should check the sume of 1 rounded and one ceiled numbers', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
   });
 });
