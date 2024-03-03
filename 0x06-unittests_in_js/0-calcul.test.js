1aade7aa89f861cf990d8d06f3b1fac189cc3900
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-03 11:26:41 +0100

    Added more tests

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -8,9 +8,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
     assert.strictEqual(calculateNumber(1.0, 3.7), 5);
     assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-  it('should check the sume of 1 rounded and one ceiled numbers', () => {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.4, 3.4), 4);
+    assert.strictEqual(calculateNumber(1.8, 3.4), 5);
   });
 });
