bea9f9f37873c0a66e087eb5ce6b0b5269b064c6
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-03 11:14:24 +0100

    Add: Second number ceil check

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -6,7 +6,7 @@ describe('calculateNumber', () => {
     assert.strictEqual(calculateNumber(1, 3), 4);
     assert.strictEqual(calculateNumber(1, 3.7), 5);
     assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+    assert.strictEqual(calculateNumber(1.0, 3.7), 5);
     assert.strictEqual(calculateNumber(1.4, 3), 4);
   });
   it('should check the sume of 1 rounded and one ceiled numbers', () => {
