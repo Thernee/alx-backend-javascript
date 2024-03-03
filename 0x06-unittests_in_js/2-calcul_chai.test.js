1b18f670fa8192adfa494970c3d1468d6ab75349
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-03 11:45:32 +0100

    Fix: test function spellings

--- 0x06-unittests_in_js/2-calcul_chai.test.js
+++ 0x06-unittests_in_js/2-calcul_chai.test.js
@@ -2,19 +2,19 @@ const calculateNumber = require('./1-calcul');
 const chai = require('chai');
 const expect = chai.expect;
 
-describe('calculateNumber', function () {
-  it('should return the sum of numbers rounded off', function () {
+describe('calculateNumber', () => {
+  it('should return the sum of numbers rounded off', () => {
     // assert.strictEqual(calculateNumber('SUM', 1.4, 4.5), 6);
-    expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equals(6)
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.be.equal(6)
   });
-  it('should return the difference of numbers rounded off', function () {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equals(-4)
+  it('should return the difference of numbers rounded off', () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.be.equal(-4)
   });
-  it('should return the division of numbers rounded off', function () {
-    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equals(0.2)
+  it('should return the division of numbers rounded off', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 4.5)).to.be.equal(0.2)
   });
 
-  it('should return "Error" if rounded b is 0', function () {
-    expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equals('Error')
+  it('should return "Error" if rounded b is 0', () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.be.equal('Error')
   });
 });
