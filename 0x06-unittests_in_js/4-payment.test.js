d30da2d8de0d144a769ae7311aa6bb1440c70477
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-03 23:08:34 +0100

    completed 4-payment.test.js implementation

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,17 +1,22 @@
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
+const sendPaymentRequestToApi = require('./4-payment');
 const Utils = require('./utils');
 
 describe('sendPaymentRequestToApi', () => {
   it('should validate the usage of the Utils function', () => {
-    const spy = sinon.spy(Utils);
-
+    const consoleLogSpy = sinon.spy(console, 'log');
+    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
+  
     sendPaymentRequestToApi(100, 20);
-    expect(spy.calculateNumber.calledOnce).to.be.true;
-    expect(spy.calculateNumber.calledWith('SUM', 100, 20)).to.be.true;
+    expect(stub.calledOnce).to.be.true;
+    expect(stub.calledWith('SUM', 100, 20)).to.be.true;
 
-    spy.calculateNumber.restore();
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith("The total is: 10")).to.be.true;
+
+    stub.restore();
+    consoleLogSpy.restore();
   });
 });
