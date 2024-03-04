66182858381993b7c117e7c8a9a0dcfeac1f8cfb
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-04 13:37:37 +0100

    Implement: test for a resolved promise

---
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -0,0 +1,15 @@
+const chai = require('chai');
+expect  = chai.expect;
+
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe("getPaymentTokenFromAPI", () => {
+  it("should return object when promise is resolved", (done) => {
+    getPaymentTokenFromAPI(true)
+      .then((result) => {
+        expect(result.data).to.be.equal('Successful response from the API')
+      });
+
+    done()
+  });
+});
