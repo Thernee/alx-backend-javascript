8854e2511290a30fb9b032a299d09bad02385fb6
Abubakar Adam Sani <Athernee@gmail.com>
2024-03-04 12:49:02 +0100

    Implement: new tests with hooks

---
+++ 0x06-unittests_in_js/5-payment.js
@@ -0,0 +1,9 @@
+// const { calculateNumber } = require('./utils');
+const Utils = require('./utils');
+
+const sendPaymentRequestToApi = (totalAmount, totalShipping) => {
+  const total = Utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+}
+
+module.exports = sendPaymentRequestToApi;
---
+++ 0x06-unittests_in_js/5-payment.test.js
@@ -0,0 +1,30 @@
+const chai = require('chai');
+const expect = chai.expect;
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./5-payment');
+
+describe('sendPaymentRequestToApi', () => {
+  let consoleLogSpy;
+
+  beforeEach(() => {
+    consoleLogSpy = sinon.spy(console, 'log');
+  });
+
+  afterEach(() => {
+    consoleLogSpy.restore();
+  });
+
+  it('should validate log function with 100, and 20', () => {
+    sendPaymentRequestToApi(100, 20);
+
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith("The total is: 120")).to.be.true;
+  });
+
+  it('should validate log function with 10, and 10', () => {  
+    sendPaymentRequestToApi(10, 10);
+
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith("The total is: 20")).to.be.true;
+  });
+});
